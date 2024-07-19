55b2bdd49b827c880affdbb67cd62dbc1e5d3d43
chejaerubinmora <104090992+chejaerubinmora@users.noreply.github.com>
2024-07-19 12:15:25 +0800

    Update con_trader_csv_import.py

--- api/trader/management/commands/con_trader_csv_import.py
+++ api/trader/management/commands/con_trader_csv_import.py
@@ -1,7 +1,13 @@
+import glob
 import logging
 import os
+import pandas as pd
 
 from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.contrib.auth.models import User
+
+from api.trader.models import Product
 
 logger = logging.getLogger('django_project_api')
 
@@ -10,5 +16,27 @@ class Command(BaseCommand):
     help = 'CSV import product'
 
     def handle(self, *args, **options):
-        home_dir = os.path.expanduser("~")
-        logger.info("Custom Management Command")
+        root_dir = os.path.dirname(settings.BASE_DIR)
+        csv_dir = os.path.join(root_dir, 'csv')
+        csv_files = glob.glob(os.path.join(csv_dir, '*.csv'))
+        df_list = [pd.read_csv(file) for file in csv_files]
+        combined_df = pd.concat(df_list, ignore_index=True)
+
+        for _, row in combined_df.iterrows():
+            user_id = row.get('user')
+            user = User.objects.filter(pk=user_id).first()
+
+            if user:
+                name = row.get('name')
+                currency = row.get('currency')
+                quantity = row.get('quantity')
+                price = row.get('price')
+
+                product_data = {
+                    'user': user,
+                    "quantity": quantity,
+                    "name": name,
+                    "currency": currency,
+                    "price": price
+                }
+                Product.objects.create(**product_data)
